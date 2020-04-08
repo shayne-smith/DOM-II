595de73a7b665c3a2182ab53f2b131f091d05896
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-07 22:41:51 -0500

    added function to flip an image, wip

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 // ---- mouseover
-// keydown
+// ---- keydown **** Come back to this one and improve
 // wheel
 // drag / drop
 // load
@@ -9,6 +9,10 @@
 // scroll
 // select
 // ---- dblclick
+// ---- mouseleave
+// ---- keyup
+// ---- click
+
 
 // Create a function called 'get'
 // that takes a CSS selector as its argument
@@ -30,11 +34,14 @@ const navLink = get('.nav-link')
 const logo = get('.logo-heading')
 
 const intro = get('.intro')
+const headerImg = get('.intro img')
 const contentSection = get('.content-section')
 const destination = get('.destination')
 
 const buttons = getAll('.btn') // creates a buttons array
 const footer = get('.footer')
+
+let onOff = 0
 
 
 // ================= FUNCTION DECLARATIONS ==================
@@ -61,12 +68,27 @@ function logoLeave(event) {
     event.target.style.color = 'black'
 }
 function dimming(event) {
-    if (event.key === "ArrowDown"){
+    if (event.key === 'ArrowDown'){
         console.log('You are hitting down arrow')
         event.target.style.opacity = 0.5
     }
 }
-
+function brightening(event) {
+    if (event.key === 'ArrowUp'){
+        console.log('You are hitting the up arrow :)')
+        event.target.style.opacity = 1
+    }
+}
+function flipImg(event) {
+    console.log('You are flipping an image :)')
+    if (onOff === 0){
+        event.target.style.transform = 'scaleX(-1)'
+        onOff = 1
+    } else if (onOff === 1){
+        event.target.style.transform = 'scaleX(1)'
+        onOff = 0
+    }
+}
 
 // ================== ADD EVENT LISTENERS ======================
 for (let i = 0; i < buttons.length; i++) {
@@ -74,9 +96,14 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('dblclick', blackText)
 }
 
-mainNav.addEventListener('mouseover', red) // add event listener to navigation to turn it red when moused over
+// add event listener to navigation to turn it red when moused over
+mainNav.addEventListener('mouseover', red) 
 
+// add event listener 
 logo.addEventListener('mouseover', logoHover)
 logo.addEventListener('mouseleave', logoLeave)
 
 body.addEventListener('keydown', dimming)
+body.addEventListener('keyup', brightening)
+
+headerImg.addEventListener('click', flipImg)
